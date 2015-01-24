399d1256d540b352518195bcabe1a44d42083aea
Marcin Brzeziński <cintrzyk@gmail.com>
2015-01-24 16:19:18 +0100

    run authorized_keys_for in rails_app cookbook

--- site-cookbooks/rails_app/recipes/default.rb
+++ site-cookbooks/rails_app/recipes/default.rb
@@ -1,6 +1,9 @@
 user = app_name = node['app_name']
 
 add_sys_user user
+add_authorized_keys_for user do
+  authorized_keys node['authorized_keys']
+end
 cap_dir_tree_for app_name
 create_db_for app_name
 nginx_site_for app_name
